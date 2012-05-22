695b45434a8821f394e2c1f11dafe7def00b1c4f
Darrin Collins <darrin.collins@tdameritrade.com>
2012-05-22 14:42:42 -0500

    Merged the custom person repository into PersonRepository.

--- src/main/java/com/example/mvc/repository/PersonRepository.java
+++ src/main/java/com/example/mvc/repository/PersonRepository.java
@@ -1,9 +1,14 @@
 package com.example.mvc.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.mvc.entity.Person;
-import com.example.mvc.repository.custom.PersonRepositoryCustom;
+import org.springframework.data.jpa.repository.Query;
 
-public interface PersonRepository extends JpaRepository<Person, Integer>,
-        PersonRepositoryCustom {
+import com.example.mvc.entity.Person;
+
+public interface PersonRepository extends JpaRepository<Person, Integer> {
+    
+    @Query("SELECT p FROM Person p WHERE name LIKE ?1")
+    Page<Person> findByNameLike(String name, Pageable pageable);
 }
